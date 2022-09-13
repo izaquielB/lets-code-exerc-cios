be2186f0d8e9e7835bbff844259551a6e0a5de68
izaquiel <izaquielbernadino@outlook.com>
2022-09-13 16:55:53 -0300

    Melhorias

--- Main.java
+++ Main.java
@@ -4,21 +4,22 @@ public class Main{
     public static void main(String[] args) {
         System.out.println("Qual fruta você deseja comprar hoje?");
         Scanner input = new Scanner(System.in);
-        String fruit = input.next().toLowerCase();
-        String resultVerify = verifyFruit(fruit);
+        String fruit = input.next();
+        boolean resultVerify = verifyFruit(fruit);
 
-        if(resultVerify.equals(fruit)) {
+        if(resultVerify) {
             System.out.println("Ótima escolha! Essa fruta está em promoção hoje.");
         } else {
             System.out.println("Que pena :( ! Essa fruta não está em promoção hoje. ");
         }
     }
-    public static String verifyFruit(String fruit) {
+    public static Boolean verifyFruit(String fruit) {
         String[] fruits = {"banana", "maça", "abacaxi", "melão", "mamão"};
-        String sale = "";
-        for (int i = 0; i < fruits.length; i++) {
-            if(fruits[i].equals(fruit)) {
-                sale = fruits[i];
+        boolean sale = false;
+        for (String s : fruits) {
+            if (s.equalsIgnoreCase(fruit)) {
+                sale = true;
+                break;
             }
         }
         return sale;
